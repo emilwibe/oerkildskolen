f181bd1108470a8dfa04390676556e0fc09d05b1
Emil Wibe <Emil@Emils-MacBook-Pro.local>
2018-06-07 14:50:38 +0200

    theme dev

--- template-parts/global-flex.php
+++ template-parts/global-flex.php
@@ -52,6 +52,65 @@
 
       </div><!--/.wrapper.module.img-->
 
+    <?php elseif(get_row_layout() == 'global_flex_text-img') : ?>
+
+      <div class="wrapper lg row lg-col-50 md-col-50 sm-col-100 module text-image">
+        <div>
+          <div>
+            <?php the_sub_field('global_flex_text-img_text'); ?>
+          </div>
+        </div>
+        <div>
+          <div>
+            <?php
+              $image = get_sub_field('global_flex_text-img_img');
+              $size = 'full';
+              if($image){
+                echo wp_get_attachment_image($image, $size);
+              }
+              unset($image, $size);
+            ?>
+          </div>
+        </div>
+      </div><!--/.wrapper.lg.row-->
+
+    <?php elseif(get_row_layout() == 'global_flex_video') : ?>
+
+      <div class="wrapper <?php the_sub_field('global_flex_video_width'); ?>">
+        <div class="videoWrapper">
+          <?php the_sub_field('global_flex_video'); ?>
+        </div><!--/.videoWrapper-->
+      </div><!--/.wrapper-->
+
+
+
+    <?php elseif(get_row_layout() == 'global_flex_img-text') : ?>
+
+      <div class="wrapper lg row lg-col-50 md-col-50 sm-col-100 module image-text">
+        <div>
+          <div>
+            <?php
+              $image = get_sub_field('global_flex_img-text_img');
+              $size = 'full';
+              if($image){
+                echo wp_get_attachment_image($image, $size);
+              }
+              unset($image, $size);
+            ?>
+          </div>
+        </div>
+        <div>
+          <div>
+            <?php the_sub_field('global_flex_img-text_text'); ?>
+          </div>
+        </div>
+      </div><!--/.wrapper.lg.row-->
+
+
+
+
+
+
     <?php endif; ?>
 
   <?php endwhile; ?>

b84fda8ea21eb1f47aca7382a0646ac3642fc515
Emil Wibe <emil@emils-imac.lan>
2018-07-04 10:12:59 +0200

    w activity

--- template-parts/flexmodule-activity.php
+++ template-parts/flexmodule-activity.php
@@ -4,10 +4,13 @@
     <ul>
   <?php while(have_rows('global_flex_date-week_list_activity')) : the_row(); ?>
 
-      <li>
-
-        hello
-
+      <li class="flex">
+        <div class="date">
+          <span class="number"><?php the_sub_field('global_flex_date-week_list-number'); ?></span>
+          <span class="week-date"><?php the_sub_field('global_flex_date-week_list-week-month'); ?></span>
+        </div>
+        <time class="time"><?php the_sub_field('global_flex_date-week_list_time'); ?></time>
+        <div class="description"><?php the_sub_field('global_flex_date-week_list-description'); ?></div>
       </li>
   <?php endwhile; ?>
 

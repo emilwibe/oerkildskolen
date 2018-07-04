e4d63d47938fceedd7ecdcc862845d5d1e4cd0a1
Emil Wibe <emil@emils-imac.lan>
2018-07-04 10:21:25 +0200

    w activity

--- template-parts/flexmodule-activity.php
+++ template-parts/flexmodule-activity.php
@@ -1,10 +1,10 @@
 <?php if(have_rows('global_flex_date-week_list_activity')) : ?>
 
-  <div class="wrapper md module activity">
+  <div class="wrapper sm module activity">
     <ul>
   <?php while(have_rows('global_flex_date-week_list_activity')) : the_row(); ?>
 
-      <li class="flex flex-wrap">
+      <li class="flex flex-wrap align-center">
         <div class="date">
           <span class="number"><?php the_sub_field('global_flex_date-week_list-number'); ?></span>
           <span class="week-date"><?php the_sub_field('global_flex_date-week_list-week-month'); ?></span>

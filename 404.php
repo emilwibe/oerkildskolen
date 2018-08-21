b6d90defad7ae76b9e1816edbff9b9dd438fde11
Emil Wibe <mail@emilwibe.dk>
2018-08-21 13:26:45 +0200

    adding 404 page

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+    <div class="wrapper md">
+        <div class="wysiwyg">
+            <h1>Fejl 404</h1>
+
+            <p>Siden du leder efter findes desværre ikke på denne adresse</p>
+        
+        </div>
+        
+
+        
+    </div>
+
+<?php get_footer(); ?>

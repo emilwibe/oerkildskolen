2d7169ddf08e1ace6c9bbefff0f9a016311f31ba
Emil Wibe <mail@emilwibe.dk>
2018-08-28 16:10:19 +0200

    inserted footer text

--- footer.php
+++ footer.php
@@ -1,8 +1,11 @@
+  <ul class="footer list">
+    <li>Ørkildskolen</li>
+    <li>Skolegade / Østre Skolevej</li>
+    <li>5700 Svendborg</li>
+    <li>62 23 63 00</li>
+    <li><a href="mailto:orkildskolen@svendborg.dk">orkildskolen@svendborg.dk</a></li>
+  </ul>
+
   <?php wp_footer(); ?>
-
-  <script>
-  
-
-  </script>
 </body>
 </html>

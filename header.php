63a307978eed88744b91b6ef1e8516f3b321fe5c
Emil Wibe <mail@emilwibe.dk>
2018-10-31 16:58:39 +0100

    news on front page

--- header.php
+++ header.php
@@ -25,7 +25,6 @@
   <meta name="msapplication-square150x150logo" content="<?php echo get_template_directory_uri(); ?>/assets/icons/mstile-150x150.png" />
   <meta name="msapplication-wide310x150logo" content="<?php echo get_template_directory_uri(); ?>/assets/icons/mstile-310x150.png" />
   <meta name="msapplication-square310x310logo" content="<?php echo get_template_directory_uri(); ?>/assets/icons/mstile-310x310.png" />
-  <?php get_template_part('template-parts/ogImage'); ?>
 
   <?php wp_head(); ?>
 </head>
--- template-parts/ogImage.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if(get_field('image_header')){
-        $image = get_field('image_header');
-
-        echo '<meta property="og:image" content="' . str_replace("ørkildskolen", "xn--rkildskolen-fgb", wp_get_attachment_url($image)) . '" />';
-      
-    }
-?>

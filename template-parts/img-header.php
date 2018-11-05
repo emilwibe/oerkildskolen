2fc9772ff835968ef9287b262f799a3e19490cba
Emil Wibe <mail@emilwibe.dk>
2018-11-05 08:33:09 +0100

    add fallback for old thumbnail images

--- template-parts/img-header.php
+++ template-parts/img-header.php
@@ -1,4 +1,16 @@
   <div class="wrapper lg header image">
-    <?php the_post_thumbnail(); ?>
+    <?php 
+      if(has_post_thumbnail()){
+        the_post_thumbnail();
+      } else {
+        
+        $image = get_field('image_header');
+        $size = 'full'; // (thumbnail, medium, large, full or custom size)
 
+        if($image){
+          echo wp_get_attachment_image( $image, $size );
+        }
+    ?>
+
+    <?php } ?>
   </div><!--/.wrapper.lg.header.img-->

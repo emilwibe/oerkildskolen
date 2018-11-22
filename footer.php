e5204166cb912cea9445c6af398457774f0c7f22
Emil Wibe <mail@emilwibe.dk>
2018-11-22 09:26:52 +0100

    change of phone number in footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <li>Ørkildskolen</li>
     <li>Skolegade / Østre Skolevej</li>
     <li>5700 Svendborg</li>
-    <li>62 23 63 00</li>
+    <li>62 23 63 01</li>
     <li><a href="mailto:orkildskolen@svendborg.dk">orkildskolen@svendborg.dk</a></li>
   </ul>
 

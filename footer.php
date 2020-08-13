97ab7213ba02ab3070211136445b5b56f95e05e3
Emil Wibe <mail@ew.dk>
2020-08-13 18:16:30 +0200

    [feat]: adding acf options page

--- footer.php
+++ footer.php
@@ -10,14 +10,6 @@
   </ul>
 <?php endif; ?>
 
-  <ul class="footer list">
-    <li>Ørkildskolen</li>
-    <li>Skolegade / Østre Skolevej</li>
-    <li>5700 Svendborg</li>
-    <li>62 23 63 01</li>
-    <li><a href="mailto:orkildskolen@svendborg.dk">orkildskolen@svendborg.dk</a></li>
-  </ul>
-
   <?php wp_footer(); ?>
 </body>
 </html>

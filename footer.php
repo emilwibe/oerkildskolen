1f8daae033791774ca01255668094ff6c398cf39
Emil Wibe <mail@ew.dk>
2020-08-13 18:15:45 +0200

    [feat]: adding acf options page

--- footer.php
+++ footer.php
@@ -1,3 +1,15 @@
+<?php if( have_rows( 'footer_content_repeater', 'option' ) ) : ?>
+
+  <ul class="footer list">
+  <?php while( have_rows( 'footer_content_repeater', 'option' ) ) : the_row(); ?>
+
+    <li><?php the_sub_field( 'text_footer_content_repeater' ); ?></li>
+
+  <?php endwhile; ?>
+
+  </ul>
+<?php endif; ?>
+
   <ul class="footer list">
     <li>Ørkildskolen</li>
     <li>Skolegade / Østre Skolevej</li>
